c6d93fefc120e5cbe4f7bd9f8f2bbcb8cf45745f
Muhammad Bilal <hmbilal8726@gmail.com>
2025-11-17 10:59:53 +0500

    test

--- ats/ats/doctype/inward_gate_pass/inward_gate_pass.js
+++ ats/ats/doctype/inward_gate_pass/inward_gate_pass.js
@@ -1,8 +1,10 @@
 // Copyright (c) 2025, Ats and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Inward Gate Pass", {
-// 	refresh(frm) {
+frappe.ui.form.on("Inward Gate Pass", {
+	// refresh(frm) {
 
-// 	},
-// });
+	// },
+});
+
+
